7cb1b1fb454a2dbe6d3b9a20ce244f93a725e3a1
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-03-10 23:34:31 +0100

    Implementation Material-UI & Add to MainLayout

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PageNav from '../PageNav/PageNav';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
 const MainLayout = ({children}) => (
   <div>
-    <PageNav />
-    <main>
-      {children}
-    </main>
+    <AppBar>
+      <Toolbar>
+        <PageNav />
+      </Toolbar>
+    </AppBar>
+    {children}
   </div>
 );
 
