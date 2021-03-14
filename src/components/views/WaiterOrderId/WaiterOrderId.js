3b20b1cc1ed83bcdfb5f9b45ef55092fba43fa03
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-03-14 23:57:44 +0100

    Add OrderId view

--- src/components/views/WaiterOrderId/WaiterOrderId.js
+++ src/components/views/WaiterOrderId/WaiterOrderId.js
@@ -1,12 +1,80 @@
 import React from 'react';
 import styles from './WaiterOrderId.module.scss';
+import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+import HomeIcon from '@material-ui/icons/Home';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
-const WaiterOrderId = () => (
-  <div className={styles.component}>
-    <h2>
-      WaiterOrderId view
-    </h2>
-  </div>
-);
+const demoOrder = [
+  {table: '1', name: 'cake', options: ['Zio Stefanos Doughnut'], amount:'1', price:'100'},
+];
+
+
+const WaiterOrderId = (props) => {
+
+  return(
+    <Paper className={styles.component}>
+      <CssBaseline />
+      <Grid container justify="space-between">
+        <Grid item xs={2}>
+          <Button component={Link} to={`${process.env.PUBLIC_URL}/waiter`}>
+            <HomeIcon />
+          </Button>
+        </Grid>
+        <Grid item xs={10} >
+          <Typography align="center" variant="h5" gutterBottom>Order {props.match.params.id}</Typography>
+        </Grid>
+      </Grid>
+
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Table</TableCell>
+            <TableCell>Menu</TableCell>
+            <TableCell>Options</TableCell>
+            <TableCell>Amount</TableCell>
+            <TableCell>Price</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {demoOrder.map(order => (
+            <TableRow key={order}>
+              <TableCell component="th" scope="row">
+                {order.table}
+              </TableCell>
+              <TableCell>
+                {order.name}
+              </TableCell>
+              <TableCell>
+                {order.options.join(',')}
+              </TableCell>
+              <TableCell>
+                {order.amount}
+              </TableCell>
+              <TableCell>
+                {order.price}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+};
+
+WaiterOrderId.propTypes = {
+  match: PropTypes.object,
+  params: PropTypes.object,
+  id: PropTypes.string,
+};
 
 export default WaiterOrderId;
